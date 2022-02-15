85079f94e1c72d1b9c13461d00bc495b14cbddca
Ovidiu2811 <90924494+Ovidiu2811@users.noreply.github.com>
2022-02-15 18:51:50 +0200

    Updates

---
+++ precache-manifest.6d5f9d93c711a754f2770b48ba68268f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29000b55e59bffeaa2db5d1d35f2910",
+    "url": "/The-Cat-Dog-Project/index.html"
+  },
+  {
+    "revision": "e91d2752bc95c0a90711",
+    "url": "/The-Cat-Dog-Project/static/css/2.c0cdccd9.chunk.css"
+  },
+  {
+    "revision": "55a58c820a906dab1ef6",
+    "url": "/The-Cat-Dog-Project/static/css/main.918a6d07.chunk.css"
+  },
+  {
+    "revision": "e91d2752bc95c0a90711",
+    "url": "/The-Cat-Dog-Project/static/js/2.46fe34d8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/The-Cat-Dog-Project/static/js/2.46fe34d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a58c820a906dab1ef6",
+    "url": "/The-Cat-Dog-Project/static/js/main.f02cc47c.chunk.js"
+  },
+  {
+    "revision": "9c7fa870b5d408f78e2a",
+    "url": "/The-Cat-Dog-Project/static/js/runtime-main.d7d954f7.js"
+  }
+]);
